f5694a78e8294cb607bbd8c5d04d6789adcc303b
hongki10 <73204664+hongki10@users.noreply.github.com>
2020-11-08 05:14:15 +0700

    Add files via upload
    Alhamdulillah

---
+++ PBOIF10K_Latihan31_PerkenalanMahasiswa/Latihan31.java
@@ -0,0 +1,17 @@
+/**
+ * Nama     : M.Hongki Alfikram
+ * Kelas    : IF10K
+ * NIM      : 10119915
+ */
+ 
+class mahasiswa {
+    public String nim;
+    public String nama;
+    
+   public void PerkenalanDiri(String nim, String nama){
+       System.out.println("Hello Everyone");
+       System.out.println("My NIM is "  + nim);
+       System.out.println("My Name is "  + nama);
+   }
+    
+}
